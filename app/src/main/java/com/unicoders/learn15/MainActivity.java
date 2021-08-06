430b0676a279507ff39426da751a6ad492fb8bef
sudharaD <sudharad7@gmail.com>
2021-08-06 08:09:30 +0530

    15 replacing 14

---
+++ app/src/main/java/com/unicoders/learn15/MainActivity.java
@@ -0,0 +1,21 @@
+package com.unicoders.learn15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello Toast", Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
